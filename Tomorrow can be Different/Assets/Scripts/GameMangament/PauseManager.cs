2250fe1d763214db20f510bfcf3275c35b834c63
JonnyTheKing-3 <jonathangrcrdrgz@gmail.com>
2024-03-11 12:32:59 -0700

    Made pause functionality. It's all in a gameobject called PauseManager
    It's a bit junky. Still need to stop the player inputs as well as maybe make some buttons for the menu. But for now, this works, we just want functionality. Later we'll polish

--- Tomorrow can be Different/Assets/Scripts/GameMangament/PauseManager.cs
+++ Tomorrow can be Different/Assets/Scripts/GameMangament/PauseManager.cs
@@ -1,18 +1,57 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PauseManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    /*
+     * Still need to pause the player Inputs. I can do it with like a boolean or something.
+     * I'll do that later. Don't wanna overcomplicate things
+     * Later I can make more Pause functions but for now, this will do
+     */
+    
+    
+    private bool isPaused = false;
+    
+    [Header("Pause settings")]
+    [SerializeField] private KeyCode pauseKey = KeyCode.Escape;
+    [SerializeField] private GameObject PausePic;
+    
 
-    // Update is called once per frame
+    
     void Update()
     {
+        if (Input.GetKeyDown(pauseKey))
+        {
+            TogglePause();
+        }
+    }
+
+    public void TogglePause()
+    {
+        isPaused = !isPaused;
+
+        if (isPaused)
+        {
+            PauseGame();
+        }
+        else
+        {
+            ResumeGame();
+        }
+    }
+    
+    public void PauseGame()
+    {
+        Time.timeScale = 0f; // Stop time
+        PausePic.SetActive(true); // Display pause menu
         
+        // Later we'll add a stop to other things if needed and undo them in resume game
+    }
+
+    public void ResumeGame()
+    {
+        Debug.Log("Resume button pressed");
+        Time.timeScale = 1f; // Resume time
+        PausePic.SetActive(false); // Stop displaying pause menu
     }
 }
+
